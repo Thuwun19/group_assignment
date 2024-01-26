a302992b60822304f8258418980a58ed0c42911f
Thuwun19 <thuwun19@gmail.com>
2024-01-27 04:01:39 +0800

    coh completed

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -13,7 +13,6 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
 deficit_record = []
 increment_record = []
 max_increment_day = {"day": None, "amount": 0}
-max_deficient = {"day": None, "amount": 0}
 current_coh = 0
 deficit_amount = 0
 deficit_day = None
@@ -31,12 +30,25 @@ for i in range (1,len(coh_data)):
             max_increment_day = {'day': day, 'amount': cash_difference}
     elif cash_difference < 0:
         deficit_record.append({"day": day, "amount": cash_difference})
-        if cash_difference < max_deficient["amount"]:
-            max_deficient = {'day': day, 'amount': cash_difference}
-print("Increment Record:", increment_record)
-print("Max Increment Day:", max_increment_day)
-print("Top Deficit Record:", deficit_record)
-print("Max Deficit Day:", sorted(max_deficient)[0:3])
+        max_deficit_day = sorted(deficit_record, key=lambda x: x["amount"], reverse=False)[0:3]
+        # deficit_record.sort(key=lambda x: x["amount"], reverse=False)
+        # max_deficit_day = sorted_deficit_record[0:3]
+
+if increment_record and deficit_record:
+    
+    for deficit in deficit_record:
+        print(f"[CASH DEFICIT] DAY:{deficit['day']} AMOUNT:{deficit['amount']}")
+    print(f"[HIGHEST CASH DEFICIT] DAY:{max_deficit_day[0]['day']} AMOUNT:{max_deficit_day[0]['amount']}")
+    print(f"[2nd HIGHEST CASH DEFICIT] DAY:{max_deficit_day[1]['day']} AMOUNT:{max_deficit_day[1]['amount']}")
+    print(f"[3rd HIGHEST CASH DEFICIT] DAY:{max_deficit_day[2]['day']} AMOUNT:{max_deficit_day[2]['amount']}")
+    
+elif increment_record:
+    print("[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")     
+    print(f"[HIGHEST CASH SURPLUS] DAY:{max_increment_day['day']} AMOUNT:{max_increment_day['amount']}")
+elif deficit_record:
+    print("[CASH DEFICIT] CASH ON EACH DAY IS LOWER THAN THE PREVIOUS DAY")     
+    print(f"[HIGHEST CASH DEFICIT] DAY:{max_deficit_day[0]['day']} AMOUNT:{max_deficit_day[0]['amount']}")
+        
 
 
 
