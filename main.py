5a96d6b405ecfe69112d4773685d66403103160a
githubnicoleee <S10259831@connect.np.edu.sg>
2024-02-01 18:36:29 +0800

    hi

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import cash_on_hand,overheads,profit_loss
 from pathlib import Path
 import csv
 ##NICOLE
-# def main():
+def main():
    
-#     output_file_path = Path.cwd() /"summary_report.txt"
-#     with output_file_path.open(mode="w", encoding="UTF-8", newline="") as output_file:
+    output_file_path = Path.cwd() /"summary_report.txt"
+    with output_file_path.open(mode="w", encoding="UTF-8", newline="") as output_file:
        
-#         output_file.write(overheads.overheads())
-#         output_file.write(cash_on_hand.coh())
-#         output_file.write(profit_loss.pl())
+        output_file.write(overheads.overheads())
+        output_file.write(cash_on_hand.coh())
+        output_file.write(profit_loss.pl())
   
-# main()
+main()
 
